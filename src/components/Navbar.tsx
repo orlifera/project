0de8f98c3b37ef75cd0bcea49b146edc30ffd5ce
orlifera <oferazzani125@gmail.com>
2024-12-08 22:06:45 +0100

    fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,6 +10,7 @@ const Navbar = () => {
             <ul className='nav-list'>
                 <li className='nav-li'><NavLink className='link nav-link' to="/">Home</NavLink></li>
                 <li className='nav-li'><NavLink className='link nav-link' to="/gallery">Galleria</NavLink></li>
+                <li className='nav-li'><NavLink className="link nav-link" to="/notes">Note</NavLink></li>
                 <li className='nav-li'><NavLink className='link nav-link' to="/wish">Wishlist</NavLink></li>
                 <li className='nav-li'><NavLink className='link nav-link' to="/watchlist">Watchlist</NavLink></li>
             </ul>
